33e7305c8b7b4c9174f531d5bd1d6144d2eee4c3
Megan <megan.miller@smoldev.dev>
2023-04-20 17:44:33 -0400

    Assessment started @ 5:20PM -- Display data on page @ 5:44PM

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,17 +1,41 @@
 <template>
   <div>
-    <h4>...todo...</h4>
-    <p>Check out the readme file for instructions.</p>
+    <h1>Nyan Cat's Performance</h1>
+    <table>
+      <tr>
+        <th>E-mail</th>
+        <th># of Tickets</th>
+        <th>Edit Guest</th>
+        <th>Delete Guest</th>
+      </tr>
+      <tr v-for="(guest, index) in guests" :key="index">
+        <td>
+          {{ guest.email }}
+        </td>
+        <td>
+          {{ guest.tickets }}
+        </td>
+        <td>Edit</td>
+        <td>Delete</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-  // eslint-disable-next-line no-unused-vars
-  const GuestRepository = require('../guest-repository');
+// eslint-disable-next-line no-unused-vars
+const GuestRepository = require("../guest-repository");
+const repo = new GuestRepository();
 
-  export default {
-    data: () => {
-      return { };
-    },
-  }
+export default {
+  data: () => {
+    return {
+      guests: [],
+    };
+  },
+  async created() {
+    this.guests = await repo.load();
+    return this.guests;
+  },
+};
 </script>
